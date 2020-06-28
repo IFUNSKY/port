8d5863410d0d53e2c03dc1b3eb57913bec12b402
IFUNSKY <ifunsaniamaka@gmail.com>
2020-06-28 18:32:55 +0100

    created homepage

--- jobs/models.py
+++ jobs/models.py
@@ -2,4 +2,4 @@ from django.db import models
 
 class Job(models.Model):
     image = models.ImageField(upload_to='images/')
-    summary = models.CharField(max_length=200, default='DEFAULT VALUE' )
+    summary = models.CharField(max_length=250, default='DEFAULT VALUE' )
